df7474f0a6999cc456b8977cb1cddc6f07fc3ab0
gibeulah <gibeulah@microsoft.com>
2018-11-29 15:06:09 +0530

    First Commit

--- RemoveDuplicatesInString/Program.cs
+++ RemoveDuplicatesInString/Program.cs
@@ -6,7 +6,42 @@ namespace RemoveDuplicatesInString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Removal of duplicates in string!");
+            Console.WriteLine("-------------------------------");
+
+            Console.WriteLine("Enter the string with duplicates");
+            String inputString = Console.ReadLine();
+            String resultantString = GetDuplicatesRemovedString(inputString);
+            Console.WriteLine();
+            if (!string.IsNullOrEmpty(resultantString)){
+                Console.WriteLine("---The resultant string is "+resultantString);
+            }else {
+                Console.WriteLine("Input string is empty!");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static string GetDuplicatesRemovedString(String inputString) {
+            if (string.IsNullOrEmpty(inputString)) {
+                return inputString;
+            }
+            if (string.IsNullOrEmpty(inputString.Trim())) {
+                return inputString.Trim();
+            }
+            char[] charArray = inputString.ToCharArray();
+            Array.Sort(charArray);
+            int newIndex = 0;
+            for (int i = 0; i < charArray.Length; i++) {
+                charArray[newIndex++] = charArray[i];
+                while (i + 1 < charArray.Length && charArray[i] == charArray[i + 1]) {
+                    i++;
+                }
+            }
+            while (newIndex < charArray.Length) {
+                charArray[newIndex++] = '\0';
+            }
+            return new string(charArray);
         }
     }
 }
